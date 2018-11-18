f0e4d7f65e3478e62c14c028e12b9d6c77fa90cf
Anatoli Trockmann <anatoli@Anatolis-MBP.fritz.box>
2018-11-18 02:30:07 +0100

    corrected path to assets

--- src/app/db/db-data.ts
+++ src/app/db/db-data.ts
@@ -8,7 +8,7 @@ const items: Item[] = [
             name: 'Hotel 1',
             type: 'hotel',
             phone: '+1285 968 685',
-            url: '/assets/img/1.jpg',
+            url: 'assets/img/1.jpg',
             address: {
                 street: 'street',
                 houseNumber: '2',
@@ -19,7 +19,7 @@ const items: Item[] = [
             name: 'Nam libero voluptatem',
             followers: 2850,
             following: 675,
-            url: '/assets/img/b1.jpg'
+            url: 'assets/img/b1.jpg'
         },
         weather: {
             name: 'Et harum quidem',
@@ -33,7 +33,7 @@ const items: Item[] = [
             name: 'Fishing 1',
             type: 'fishing',
             phone: '+1285 968 686',
-            url: '/assets/img/1.jpg',
+            url: 'assets/img/1.jpg',
             address: {
                 street: 'Munic street',
                 houseNumber: '3',
@@ -44,7 +44,7 @@ const items: Item[] = [
             name: 'Nam libero voluptatem1',
             followers: 2851,
             following: 676,
-            url: '/assets/img/b1.jpg'
+            url: 'assets/img/b1.jpg'
         },
         weather: {
             name: 'Et harum quidem',
@@ -58,7 +58,7 @@ const items: Item[] = [
             name: 'Fishing 2',
             type: 'fishing',
             phone: '+1285 968 687',
-            url: '/assets/img/1.jpg',
+            url: 'assets/img/1.jpg',
             address: {
                 street: 'Munic street1',
                 houseNumber: '4',
@@ -69,7 +69,7 @@ const items: Item[] = [
             name: 'Nam libero voluptatem2',
             followers: 2852,
             following: 677,
-            url: '/assets/img/b1.jpg'
+            url: 'assets/img/b1.jpg'
         },
         weather: {
             name: 'Et harum quidem',
@@ -83,7 +83,7 @@ const items: Item[] = [
             name: 'Tours 1',
             type: 'tours',
             phone: '+1285 968 688',
-            url: '/assets/img/1.jpg',
+            url: 'assets/img/1.jpg',
             address: {
                 street: 'Pforzheim street1',
                 houseNumber: '34',
@@ -94,7 +94,7 @@ const items: Item[] = [
             name: 'Nam libero voluptatem3',
             followers: 2853,
             following: 678,
-            url: '/assets/img/b1.jpg'
+            url: 'assets/img/b1.jpg'
         },
         weather: {
             name: 'Et harum quidem2',
